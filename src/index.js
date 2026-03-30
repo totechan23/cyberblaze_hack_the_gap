8523d2afadf0076ef5502508990ecee38b32fea5
arayan <aryanb23aryan@gmail.com>
2026-03-30 07:51:40 +0000

    yup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// frontend/src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/main.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
